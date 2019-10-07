4e00a0e0b21845f59eda49e23869752f021ee843
yugangzhang <yuzhang@bnl.gov>
2019-10-07 10:38:45 -0400

    update setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from __future__ import (absolute_import, division, print_function)
+
+import sys
+import warnings
+import versioneer
+
+from setuptools import setup, find_packages
+
+no_git_reqs = []
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+    for r in required:
+        if not (r.startswith('git') or r.startswith('#') or r.strip() == ''):
+            no_git_reqs.append(r)
+
+setup(
+    name='pyH5_GUI',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author='Brookhaven National Laboratory Yugang Zhang, yuzhang@bnl.gov',
+    packages=find_packages(),
+    install_requires=no_git_reqs,
+)
