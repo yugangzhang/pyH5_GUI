3a64c5856d861aa7973da9f96d5aa5aefd735a6d
yugangzhang <yuzhang@bnl.gov>
2019-11-23 21:15:00 -0500

    Dev new feature in tree

--- pyH5_GUI/H5Tree.py
+++ pyH5_GUI/H5Tree.py
@@ -91,11 +91,6 @@ class tree(QWidget):
         self.tree.setColumnWidth(1,0)
         self.tree.setColumnWidth(2,0)
 
-        #self.add_branch(self.tree_root,self.f)
-        #self.tree.itemClicked.connect(self.onItemClicked)
-        #self.setLayout(self.datalayout)
-        #self.show()
-
     def add_branch(self,tree_root,h5file):
         for _ in h5file.keys():
             #print(_)
@@ -110,94 +105,6 @@ class tree(QWidget):
     def onItemClicked(self,item):
         print(self.filename,item.text(2))
         
-        
-class titledTree():
-    def __init__(self, title):
-
-        #self.list = QTreeWidget()
-
-        self.tree = QTreeWidget()
-        self.title = QLabel(title)
-        #self.list.setHeaderLabel(str(title))
-        self.tree.header().close()
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.title)
-        self.layout.addWidget(self.tree)
-
-        self.grandpa_list = {}
-        self.parent_list = {}# []
-        self.child_list = {}
-
-        self.group_list = []
-        self.has_attrs_list = []
-
-
-    def clear(self):
-        self.row_list = {} # []
-        self.group_list = []
-        self.has_attrs_list = []
-        self.tree.clear()
-
-    def add_filename(self, sfilename, grandpa_name=None ):
-        if grandpa_name is None:
-            self.parent_list[sfilename] = QTreeWidgetItem(self.tree)
-            self.parent_list[sfilename].setText(0,  sfilename )
-            self.child_list[sfilename] = [ ]
-        else:
-            if grandpa_name not in list(self.grandpa_list.keys()):
-                self.grandpa_list[grandpa_name] = QTreeWidgetItem(self.tree)
-                self.grandpa_list[grandpa_name].setText(0,  grandpa_name )
-            #print(  sfilename, grandpa_name, self.grandpa_list[grandpa_name] )    
-            self.parent_list[sfilename] = QTreeWidgetItem(  self.grandpa_list[grandpa_name]   )
-            self.parent_list[sfilename].setText(0,  sfilename )
-            self.child_list[sfilename] = [ ]
-
-    def add_item(self, parent_index, item, hdf5_file, sfilename ):
-
-        parent = self.parent_list[sfilename]
-        item_text = item.split('/')[-1]
-        #self.row_list[-1].setText(0, item_text)
-        is_group = isinstance(hdf5_file[item], h5py.Group)
-        self.group_list.append(is_group)
-
-        has_attrs = len(list(hdf5_file[item].attrs.keys())) > 0
-        self.has_attrs_list.append(has_attrs)
-
-        if parent_index == None:
-            #Prt.append(QTreeWidgetItem(self.list))
-            child = QTreeWidgetItem( parent )
-            child.setText(0,  item_text )
-            self.child_list[sfilename].append( child )
-        else:
-            self.child_list[sfilename].append( self.child_list[sfilename][-1] )
-            self.child_list[sfilename][-1].setText(0,  item_text )
-
-
-    def full_item_path(self, selected_row):
-        text = selected_row.text(0)
-        parent_row = selected_row.parent()
-
-        while not parent_row == None:
-            text = parent_row.text(0) + '/' + text
-            parent_row = parent_row.parent()
-
-        return text
-
-
-    def swap_group_icon(self):
-        for i in range(len(self.row_list)):
-            if self.row_list[i].isExpanded():
-                if self.has_attrs_list[i]:
-                        self.row_list[i].setIcon(0, self.icon_open_group_with_attrs)
-                else:
-                    self.row_list[i].setIcon(0, self.icon_open_group)
-
-            elif self.group_list[i] == True:
-                if self.has_attrs_list[i]:
-                    self.row_list[i].setIcon(0, self.icon_closed_group_with_attrs)
-                else:
-                    self.row_list[i].setIcon(0, self.icon_closed_group)
-                #self.row_list[i].setIcon(0, self.closed_group_icon)
 
 
 class titledTable():
@@ -232,3 +139,5 @@ class titledTable():
             numcols = value_shape[1]
 
         return numcols
+
+
